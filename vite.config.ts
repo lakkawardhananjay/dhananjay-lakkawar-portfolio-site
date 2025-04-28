6b3f419844c82c95ac1472351ded6a36efdd68cc
Dhananjay lakkawar <92675267+lakkawardhananjay@users.noreply.github.com>
2025-04-28 17:48:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react';
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  
   server: {
     host: "::",
     port: 8080,
@@ -14,6 +16,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/dhananjay-lakkawar-portfolio-site/'
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
